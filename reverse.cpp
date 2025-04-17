31ade7cac187e6d5657f22e9590ca3b55bb4b075
9534495797 <chaubeyankit927@gmail.com>
2025-04-17 22:17:44 +0530

    ok

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+
+
+	int num;
+	cin>>num;
+	int rem;
+	int rev=0;
+	while(num!=0){
+		 rem=num%10;
+	 rev=rev*10+rem;
+		num/=10;
+	}
+	cout<<rev;
+	
+}
